2e2633c64cfdf4654073d7a5a8781b20fe4b91b3
newellmatt <mnewell10@gmail.com>
2016-04-06 11:35:20 -0700

    Add ability for user to see Total Cost

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  items: ["Bauer NME 7 Goalie Helmet"],
+
+  items: [],
+  totalPrice: Ember.computed('items.[]', function(){
+    var items = this.get('items');
+    console.log('computing');
+    var totalPrice = 0;
+    items.forEach(function(item) {
+      totalPrice = totalPrice + parseInt(item.get('cost'));
+    });
+    return totalPrice;
+  }),
 
   add(item) {
     this.get('items').pushObject(item);
@@ -14,4 +24,5 @@ export default Ember.Service.extend({
   includes(item) {
     return this.get('items').includes(item);
   },
+
 });
