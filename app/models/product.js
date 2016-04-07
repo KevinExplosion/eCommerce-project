43a2698e943153dc3271ef32bb790974185965db
kevin <mattisonii@yahoo.com>
2016-04-07 15:54:16 -0700

    Add styles.

--- app/models/product.js
+++ app/models/product.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   title: DS.attr(),
   image: DS.attr(),
   description: DS.attr(),
-  cost: DS.attr()
+  cost: DS.attr(),
+  created: DS.attr('date', {defaultValue() { return new Date(); } }),
 });
