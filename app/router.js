d2b936b08ebc63f060bb83d7822038cc24fd451d
kevin <mattisonii@yahoo.com>
2016-04-07 14:07:59 -0700

    Add product detail link.

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('store');
   this.route('shopping-cart');
-  this.route('store-item', {path: '/store-item/:store_item_id'});
+  this.route('store-item', {path: '/store-item/:product_id'});
   this.route('signin');
   this.route('sign-up');
   this.route('welcome');
--- app/routes/store-item.js
+++ app/routes/store-item.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('store_item', params.store_item);
+    return this.store.findRecord('product', params.product_id);
   },
 });
